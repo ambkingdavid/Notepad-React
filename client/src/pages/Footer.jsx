2de07ec9aac32d3b6a19e099995839bef67282e0
ambkingdavid <kelvinwooldbridge@gmail.com>
2024-01-22 18:10:04 +0100

    update the footer

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer
             className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/"
-                className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                className="hover:underline">NotePad™</a>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 space-x-3">
                 <li>
