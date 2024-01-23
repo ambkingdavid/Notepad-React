d9395ba3c3b93fbdd5e9930866d9af8afefb943b
ambkingdavid <kelvinwooldbridge@gmail.com>
2024-01-23 14:10:16 +0100

    server config

--- server/app.js
+++ server/app.js
@@ -54,7 +54,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173',
+  origin: 'https://notepad-bpa9.onrender.com',
   methods: 'GET,PUT,POST,DELETE'
 }));
 app.use(morgan('dev'));
